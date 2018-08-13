ee968327a23f7883943800a25faa21baa7044443
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 18:15:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,17 +36,6 @@ end
 def hit? (number)
   # code hit? here
   prompt_user
-  
-  if get_user_input == 'h'
-    deal_card
-    
-  elsif get_user_input == 's'
-  
-  else 
-    invalid_command
-    
-end
-
 end 
 
 def invalid_command
