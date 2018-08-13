bb4df74912171e2ed2f1436afb9c4b2e05f15309
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 17:59:57 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,9 +30,9 @@ end
 
 def initial_round
   # code #initial_round here
-  sum = deal_card
-  sum += deal_card
-  display_card_total(sum)
+  deal_card
+  deal_card
+  display_card_total()
 end
 
 def hit?
